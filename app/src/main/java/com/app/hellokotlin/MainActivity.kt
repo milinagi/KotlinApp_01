d540ff0e3af40f1e4ae759ce608751a38def26d5
JorgeRC <jorgercdeveloper@gmail.com>
2022-09-06 11:25:06 +0100

    Initial commit

---
+++ app/src/main/java/com/app/hellokotlin/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.app.hellokotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textViewStart = findViewById<TextView>(R.id.textViewStart)
+        val textViewHello = findViewById<TextView>(R.id.textViewHello)
+
+        findViewById<Button>(R.id.buttonReady).setOnClickListener {
+
+            textViewStart.visibility = View.VISIBLE
+            textViewHello.text = "Empezando Curso!!!"
+
+            /* findViewById<TextView>(R.id.textViewHello).text = "Empezando Curso!!" */
+        }
+    }
+}
